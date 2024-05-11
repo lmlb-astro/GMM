6bcaa52c9768007c28fff15758fe44723d59436d
lmlb-astro <136018556+lmlb-astro@users.noreply.github.com>
2024-05-10 20:50:27 -0700

    GMM fitting in function

--- GMM_functions.py
+++ GMM_functions.py
@@ -2,15 +2,25 @@
 
 
 import numpy as np
+import time
 
 import astropy.io.fits as pyfits
 import astropy.wcs as wcs
 
+from sklearn.mixture import GaussianMixture
+
 import velocity_axis_datacube as vax
+
+#### parameters related to fit_GMM ####
+
+seed_val = 312
+threshold = 0.001
+gmm_iter = 1000
+
+#####################################
 
 
 ## prepare the data for input into the GMM model
-
 def prepare_data(data, min_vel, max_vel, crval, dv, crpix):
     
     ## resample data, so that the GMM can handle it
@@ -39,6 +49,7 @@ def prepare_data(data, min_vel, max_vel, crval, dv, crpix):
     return index_arr, data_resample
 
 
+
 ## Maps the different clusters on the observed map in different colors
 def map_spatial_cluster_distribution(cluster_inds_arr, index_arr, len_x_orig_map, len_y_orig_map):
     ## initialize the map for the cluster tags
@@ -53,6 +64,42 @@ def map_spatial_cluster_distribution(cluster_inds_arr, index_arr, len_x_orig_map
     
     return cluster_map
 
+
+
+## loop over number of clusters for the GMM modeling
+def fit_GMM(data_in, n_comps_min, n_comps_max):
+    ## initialize the storage lists
+    n_comps_list = []
+    time_list = []
+    bic_list = []
+    bic_min = None
+    best_model = None
+    
+    ## loop over the number of components to be fitted
+    for i in range(n_comps_min, n_comps_max):
+        ## perform the GMM fitting
+        print("Calculating GMM for {index} number of components".format(index = i))
+        start_time = time.time()
+        temp_model = GaussianMixture(n_components = i, 
+                                     init_params = 'kmeans', 
+                                     covariance_type = 'full', 
+                                     random_state = seed_val, 
+                                     tol = threshold, 
+                                     max_iter = gmm_iter).fit(data_in)
+        end_time = time.time()
+        time_list.append(end_time - start_time)
+        
+        ## calculate the Bayesian Information Criterion (BIC)
+        temp_bic = temp_model.bic(data_in)
+        
+        ## store the best fitting model
+        if(i == n_comps_min or temp_bic < bic_min):
+            best_model = temp_model
+            bic_min = temp_bic
+        n_comps_list.append(i)
+        bic_list.append(temp_bic)
+        
+    return n_comps_list, bic_list, time_list, best_model
 
 
 
